a92cfa6cd96a3fa2c348010a727aafe2a9c35ece
hoangtrung <trungnh@solashi.com>
2025-02-17 10:40:57 +0700

    feat: add stale time to current IP query

--- src/views/Dashboard/hooks/useCurrentIP.ts
+++ src/views/Dashboard/hooks/useCurrentIP.ts
@@ -2,5 +2,7 @@ import { fetchIp } from "@/lib/utils/hash";
 import { useQuery } from "@tanstack/react-query";
 
 export const useCurrentIP = () => {
-  return useQuery(["current-ip"], fetchIp);
+  return useQuery(["current-ip"], fetchIp, {
+    staleTime: 1000 * 60 * 60 * 24,
+  });
 };
